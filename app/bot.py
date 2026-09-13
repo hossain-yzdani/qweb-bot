2952a9db8825e16e44d0244fd8d28a463d2642a7
HossainYazdani <hossain.yazdaniii@gmail.com>
2026-09-13 23:34:49 +0330

    future: add VE

--- app/bot.py
+++ app/bot.py
@@ -5,7 +5,6 @@ from aiogram import Bot, Dispatcher
 from app.config import BOT_TOKEN
 from app.handlers.commands import router
 
-
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
